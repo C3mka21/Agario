12afd139febd58c5f673d7e69e721ba78bf6c7f1
C3mka21 <artem9003@list.ru>
2021-10-11 23:06:31 +0300

    beta version

--- Game1_3.cpp
+++ Game1_3.cpp
@@ -1,6 +1,7 @@
 #include "TxLib.h"
 #include <math.h>
 
+
 int n=500;
 
 typedef struct // ball_t
@@ -30,73 +31,102 @@ typedef struct // box_t
     int lin;
     COLORREF room;
     COLORREF wall;
+    COLORREF text;
+    int x;
 } box_t;
 
 void bx (box_t rt);
 void shar(ball_t b);
 void izm (ball_t* b, box_t rt);
 void eat(ball_t* b, ball_t* b1);
+void menu(box_t* tb);
 
 int main(void)
 {
-
-    box_t rt1={350,100,1350,700,3,TX_WHITE,TX_BLACK};
-    ball_t b0[n];
-    ball_t b1={rand()%(rt1.x2-rt1.x1)+rt1.x1,rand()%(rt1.y2-rt1.y1)+rt1.y1,5,2,2,2,1,TX_RED,TX_BLACK,'W','A','S','D'};
-    ball_t b2={rand()%(rt1.x2-rt1.x1)+rt1.x1,rand()%(rt1.y2-rt1.y1)+rt1.y1,5,2,2,2,1,TX_GREEN,TX_BLACK,VK_UP,VK_LEFT,VK_DOWN,VK_RIGHT};
-    int i;
-    for(i=0;i<n;i++)
-        b0[i]={rand()%(rt1.x2-rt1.x1)+rt1.x1,rand()%(rt1.y2-rt1.y1)+rt1.y1,2,0,0,1,1,RGB(rand()%256,rand()%256,rand()%256)};
+    //box_t tb1=
     int x=1500, y=750;
     txCreateWindow(x,y);
+    txSetFillColor(TX_WHITE);
     txClear();
     txBegin();
     while(!GetAsyncKeyState(VK_ESCAPE))
     {
-        bx(rt1);
-        txSetFillColor(TX_BLACK);
-        txSelectFont ("Comic Sans MS", 40);
-        txTextOut (150, 100, "Player 2 Green");
-        txTextOut (150, 200, "Player 1 Red");
+        box_t rt1={350,150,1350,700,3,TX_WHITE,TX_BLACK};
+        ball_t b0[n];
+        ball_t b1={rand()%(rt1.x2-rt1.x1)+rt1.x1,rand()%(rt1.y2-rt1.y1)+rt1.y1,5,2,2,2,1,TX_RED,TX_BLACK,'W','A','S','D'};
+        ball_t b2={rand()%(rt1.x2-rt1.x1)+rt1.x1,rand()%(rt1.y2-rt1.y1)+rt1.y1,5,2,2,2,1,TX_GREEN,TX_BLACK,VK_UP,VK_LEFT,VK_DOWN,VK_RIGHT};
+        int i;
         for(i=0;i<n;i++)
+        b0[i]={rand()%(rt1.x2-rt1.x1)+rt1.x1,rand()%(rt1.y2-rt1.y1)+rt1.y1,2,0,0,1,1,RGB(rand()%256,rand()%256,rand()%256)};
+        while(!GetAsyncKeyState(VK_SPACE))
         {
-            eat(&b1,&b0[i]);
-            eat(&b2,&b0[i]);
-            if(b0[i].m!=0)
+            //menu(tb1);
+            bx(rt1);
+            txSetFillColor(TX_WHITE);
+            txSelectFont ("Comic Sans MS", 40);
+            txTextOut (150, 400, "Player 2 Green");
+            txTextOut (150, 200, "Player 1 Red");
+            //υπώ υπώπώπυπώ(υπώυπώ.υπώυπώ,υπώ->υπώ.υπώ);
+            txTextOut(300,100,"Press SPACE to restart");
+            txTextOut(750,100,"Press ESCAPE+SPACE to exit");
+            for(i=0;i<n;i++)
             {
-                shar(b0[i]);
+                eat(&b1,&b0[i]);
+                eat(&b2,&b0[i]);
+                if(b0[i].m!=0)
+                {
+                    shar(b0[i]);
+                }
+            }
+            shar(b1);
+            shar(b2);
+            izm(&b1,rt1);
+            izm(&b2,rt1);
+            txSleep(1);
+            eat(&b2,&b1);
+            if(b1.m==0)
+            {
+                txClear();
+                txSetFillColor(TX_WHITE);
+                txSelectFont ("Comic Sans MS", 40);
+                txTextOut (150, 400, "Player 2 Green");
+                txTextOut (150, 200, "Player 1 Red");
+                txTextOut (x/2-150, y/2-50, "Player 2 won");
+                txSleep(2500);
+                txClear();
+                txTextOut(300,100,"Press SPACE to restart");
+                txTextOut(750,100,"Press ESCAPE+SPACE to exit");
+                break;
+            }
+            eat(&b1,&b2);
+            if(b2.m==0)
+            {
+                txClear();
+                txSetFillColor(TX_WHITE);
+                txSelectFont ("Comic Sans MS", 40);
+                txTextOut (150, 400, "Player 2 Green");
+                txTextOut (150, 200, "Player 1 Red");
+                txTextOut (x/2-150, y/2-50, "Player 1 won");
+                txSleep(2500);
+                txClear();
+                txTextOut(300,100,"Press SPACE to restart");
+                txTextOut(750,100,"Press ESCAPE+SPACE to exit");
+                break;
             }
         }
-        shar(b1);
-        shar(b2);
-        izm(&b1,rt1);
-        izm(&b2,rt1);
-        txSleep(1);
-        eat(&b2,&b1);
-        if(b1.m==0)
-        {
-            txClear();
-            txSetFillColor(TX_BLACK);
-            txSelectFont ("Comic Sans MS", 40);
-            txTextOut (150, 100, "Player 2 Green");
-            txTextOut (150, 200, "Player 1 Red");
-            txTextOut (x/2-150, y/2-50, "Player 2 won");
-            return 2;
-        }
-        eat(&b1,&b2);
-        if(b2.m==0)
-        {
-            txClear();
-            txSetFillColor(TX_BLACK);
-            txSelectFont ("Comic Sans MS", 40);
-            txTextOut (150, 100, "Player 2 Green");
-            txTextOut (150, 200, "Player 1 Red");
-            txTextOut (x/2-150, y/2-50, "Player 1 won");
-            return 1;
-        }
-        txClear();
     }
     return 0;
+}
+
+void menu(box_t* tb)
+{
+    txSetFillColor(tb->room);
+    txSetColor(tb->wall,tb->lin);
+    txRectangle(tb->x1,tb->y1,tb->x2,tb->y2);
+    txSetColor(tb->text);
+    txSelectFont ("Comic Sans MS", tb->x);
+    txTextOut (tb->x2-tb->x1, tb->y2-tb->y1, "START");
+
 }
 
 void eat(ball_t* b, ball_t* b1)
